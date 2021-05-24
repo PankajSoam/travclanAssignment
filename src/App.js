cb39e32904e661647c55e19190da6e6e15f8d9c0
Pankaj Soam <pankaj.soam111@gmail.com>
2021-05-24 12:29:58 +0530

    fetching and storing data from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 
+
 function App(){
+
+    // initializing state to store fetched data from api
+    const [userData, setUserData] = useState(0);     
+     
+    // fetching data and updating state in userData
+    useEffect(() => {
+        fetch('https://intense-tor-76305.herokuapp.com/merchants')
+        .then(res => res.json())
+        .then(result => {
+            setUserData({result});
+            console.log({userData});
+        });
+    },[]);
+
+
     return(
         <h1>Initialize</h1>
     );
